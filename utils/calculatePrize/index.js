ade63143e3a5ae7bbbc84aa99e3b444c29bd94c9
serhanelmali <serhanelmali@gmail.com>
2021-10-17 00:40:39 +0300

    adds: calculatePrize fn calculates all prizes

--- utils/calculatePrize/index.js
+++ utils/calculatePrize/index.js
@@ -14,39 +14,72 @@ const calculatePrize = (amount, balance, currency, slotResults) => {
           const winPot = amount * 200
           balance = balance + winPot
           console.log(
-            `YOU WIN! 
-            Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
           )
         } else if (firstLine === 1 || firstLine === 2) {
           const winPot = amount * 100
           balance = balance + winPot
           console.log(
-            `YOU WIN! 
-            Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
           )
         } else if (firstLine === 3) {
           const winPot = amount * 18
           balance = balance + winPot
           console.log(
-            `YOU WIN! ${(firstLine, secondLine, thirdLine)}
-            Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
           )
         } else if (firstLine === 4) {
           const winPot = amount * 14
           balance = balance + winPot
           console.log(
-            `YOU WIN!
-            Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
           )
         } else if (firstLine === 5) {
           const winPot = amount * 10
           balance = balance + winPot
           console.log(
-            `YOU WIN!
-            Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
           )
         }
-      }
+      } else if (isAllEqual([firstLine, secondLine]) && thirdLine === 1) {
+        if (firstLine === 2) {
+          const winPot = amount * 100
+          balance = balance + winPot
+          console.log(
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+          )
+        } else if (firstLine === 3) {
+          const winPot = amount * 18
+          balance = balance + winPot
+          console.log(
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+          )
+        } else if (firstLine === 4) {
+          const winPot = amount * 14
+          balance = balance + winPot
+          console.log(
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+          )
+        } else if (firstLine === 5) {
+          const winPot = amount * 10
+          balance = balance + winPot
+          console.log(
+            `YOU WIN! Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+          )
+        }
+      } else if (isAllEqual([firstLine, secondLine]) && firstLine === 6) {
+        const winPot = amount * 5
+        balance = balance + winPot
+        console.log(
+          `YOU WIN!  Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+        )
+      } else if (firstLine === 6) {
+        const winPot = amount * 2
+        balance = balance + winPot
+        console.log(
+          `YOU WIN!  Added ${winPot} ${currency}s to your balance! New balance is: ${balance} ${currency}s`
+        )
+      } else console.log('NO POT')
     }
   } else {
     console.log('not enough balance')
